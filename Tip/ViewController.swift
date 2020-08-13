adc49450743328d05f2886f8896ea3196cb2f41c
Prachal80 <prachaljpatel@gmail.com>
2020-08-12 23:39:59 -0700

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Prachal Patel on 8/12/20.
+//  Copyright © 2020 Prachal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
